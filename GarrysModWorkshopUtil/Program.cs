d0c73800956a1b40ca456926334b5de22cf87563
H----- <42122270+DefyTheRush@users.noreply.github.com>
2020-01-15 08:53:29 -0500

    Program can now run only one instance at a time
    To prevent issues with memory or such

--- GarrysModWorkshopUtil/Program.cs
+++ GarrysModWorkshopUtil/Program.cs
@@ -14,6 +14,17 @@ namespace GarrysModWorkshopUtility
         [STAThread]
         static void Main()
         {
+            const string appName = "Garry's Mod Workshop Utility";
+            bool createdNew;
+
+            Mutex mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("You cannot have more than one instance of this program running", "Already running");
+                return;
+            }
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmGarrysModWorkshopUtility());
