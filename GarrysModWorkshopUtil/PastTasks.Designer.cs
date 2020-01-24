4574199c28ead8b74244fa6f64f8078f421c2e24
H----- <42122270+DefyTheRush@users.noreply.github.com>
2020-01-24 10:07:40 -0500

    Add class for viewing task history and design

---
+++ GarrysModWorkshopUtil/PastTasks.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GarrysModWorkshopUtil
+{
+    partial class PastTasks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PastTasks));
+            this.lblDeletedTasks = new System.Windows.Forms.Label();
+            this.lstDeletedTasks = new System.Windows.Forms.ListBox();
+            this.btnViewTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDeletedTasks
+            // 
+            this.lblDeletedTasks.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeletedTasks.Location = new System.Drawing.Point(171, 5);
+            this.lblDeletedTasks.Name = "lblDeletedTasks";
+            this.lblDeletedTasks.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblDeletedTasks.Size = new System.Drawing.Size(267, 43);
+            this.lblDeletedTasks.TabIndex = 1;
+            this.lblDeletedTasks.Text = "Task History";
+            this.lblDeletedTasks.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstDeletedTasks
+            // 
+            this.lstDeletedTasks.FormattingEnabled = true;
+            this.lstDeletedTasks.Location = new System.Drawing.Point(12, 53);
+            this.lstDeletedTasks.Name = "lstDeletedTasks";
+            this.lstDeletedTasks.Size = new System.Drawing.Size(600, 212);
+            this.lstDeletedTasks.TabIndex = 2;
+            // 
+            // btnViewTask
+            // 
+            this.btnViewTask.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewTask.Location = new System.Drawing.Point(240, 275);
+            this.btnViewTask.Name = "btnViewTask";
+            this.btnViewTask.Size = new System.Drawing.Size(149, 27);
+            this.btnViewTask.TabIndex = 32;
+            this.btnViewTask.Text = "View Task Information";
+            this.btnViewTask.UseVisualStyleBackColor = true;
+            this.btnViewTask.Click += new System.EventHandler(this.btnViewTask_Click);
+            // 
+            // PastTasks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 313);
+            this.Controls.Add(this.btnViewTask);
+            this.Controls.Add(this.lstDeletedTasks);
+            this.Controls.Add(this.lblDeletedTasks);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PastTasks";
+            this.Text = "Past Tasks";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PastTasks_FormClosing);
+            this.Load += new System.EventHandler(this.frmPastTasks_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDeletedTasks;
+        private System.Windows.Forms.ListBox lstDeletedTasks;
+        private System.Windows.Forms.Button btnViewTask;
+    }
+}
---
+++ GarrysModWorkshopUtil/PastTasks.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GarrysModWorkshopUtil
+{
+    public partial class PastTasks : Form
+    {
+        public ArrayList allTasks;
+        public Form sendBackToMain;
+
+        public static bool taskWindowIsOpen = false;
+
+        public PastTasks(ArrayList tasks)
+        {
+            InitializeComponent();
+            allTasks = tasks;
+        }
+
+        private void frmPastTasks_Load(object sender, EventArgs e)
+        {
+            taskWindowIsOpen = true;
+            foreach (GarrysModWorkshopUtil.Task task in allTasks)
+            {
+                lstDeletedTasks.Items.Add(task);
+            }
+        }
+
+        private void PastTasks_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            taskWindowIsOpen = false;
+        }
+
+        private void btnPermanentlyDeleteTask_Click(object sender, EventArgs e)
+        {
+            if (lstDeletedTasks.SelectedIndex < 0 || lstDeletedTasks.SelectedIndex > lstDeletedTasks.Items.Count)
+            {
+                MessageBox.Show("Please select a valid task in the list!", "Error");
+            }
+        }
+
+        private void btnViewTask_Click(object sender, EventArgs e)
+        {
+            if (lstDeletedTasks.SelectedIndex < 0 || lstDeletedTasks.SelectedIndex > lstDeletedTasks.Items.Count)
+            {
+                MessageBox.Show("Please select a valid task in the list!", "Error");
+            }
+            else
+            {
+                GarrysModWorkshopUtil.Task testTask = (GarrysModWorkshopUtil.Task)allTasks[0];
+                if (testTask.getName().Equals("Create .JSON"))
+                {
+                    string wildcards = "";
+                    foreach (String wildcard in testTask.wildCards)
+                    {
+                        wildcards += wildcard.ToString() + ",";
+                    }
+                    wildcards = wildcards.Substring(0, wildcards.LastIndexOf(","));
+                    MessageBox.Show("Addon Output: " + testTask.jsonLocation + "\n\nAddon Title: " + testTask.addonTitle + "\n\nAddon Type: " + testTask.addonType + "\n\nAddon Tags: " + testTask.addonTag1 + " & " + testTask.addonTag2 + "\n\nWildcards: " + wildcards + "\n\nNotes: " + testTask.TaskNotes, "Create JSON Task Info");
+                }
+                else if (testTask.getName().Equals("Create .GMA"))
+                {
+                    MessageBox.Show("Addon Input: " + testTask.addonLocation + "\n\nGMad.EXE Location: " + testTask.gMadLocation + "\n\nGMA Filename: " + testTask.gmaName + "\n\nGMA Output: " + testTask.gmaOutput + "\n\nWanted to Delete JSON? " + testTask.wantsToDeleteJSON + "\n\nNotes: " + testTask.TaskNotes, "Create .GMA Task Info");
+                }
+                else if (testTask.getName().Equals("Extract .GMA"))
+                {
+                    MessageBox.Show("Addon Output: " + testTask.addonLocation + "\n\nGMad.EXE Location: " + testTask.gMadLocation + "\n\nGMA Filename: " + testTask.gmaName + "\n\nGMA Location: " + testTask.gmaLocation + "\n\nNotes: " + testTask.TaskNotes, "Extract .GMA Task Info");
+                }
+                else if (testTask.getName().Equals("Publish Addon"))
+                {
+                    MessageBox.Show("Icon Location: " + testTask.iconLocation + "\n\nGMPublish.EXE Location: " + testTask.gMadLocation + "\n\nGMA Filename: " + testTask.gmaName + "\n\nGMA Location: " + testTask.gmaLocation + "\n\nNotes: " + testTask.TaskNotes, "Publish Addon Task Info");
+                }
+                else if (testTask.getName().Equals("Update Addon"))
+                {
+                    MessageBox.Show("GMPublish.EXE Location: " + testTask.gMadLocation + "\n\nGMA Filename: " + testTask.gmaName + "\n\nGMA Location: " + testTask.gmaLocation + "\n\nAddon ID: " + testTask.addonID + "\n\nChange Notes" + testTask.changeNotes + "\n\nNotes: " + testTask.TaskNotes, "Update Addon Task Info");
+                }
+                else if (testTask.getName().Equals("Update Icon"))
+                {
+                    MessageBox.Show("Icon Location: " + testTask.iconLocation + "\n\nGMPublish.EXE Location: " + testTask.gMadLocation + "\n\nAddon ID: " + testTask.addonID + "\n\nNotes: " + testTask.TaskNotes, "Update Addon Task Info");
+                }
+            }
+        }
+    }
+}
